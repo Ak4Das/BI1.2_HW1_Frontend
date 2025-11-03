fd765ccb84d519edf234473dafc214134f9834ce
Akash Das <akashdas02052@gmail.com>
2025-11-03 08:47:48 +0530

    feat: Add delete button

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,17 +1,45 @@
 import useFetch from "../useFetch"
+import { useState } from "react"
 
 const Books = () => {
-  const { data, loading, error } = useFetch("https://book-data-backend.vercel.app/books")
+  const [successMessage, setSuccessMessage] = useState("")
+  const { data, loading, error } = useFetch(
+    "https://book-data-backend.vercel.app/books"
+  )
   // console.log(data)
+
+  const deleteABook = async (bookId) => {
+    try {
+      const response = await fetch(
+        `https://book-data-backend.vercel.app/books/${bookId}`,
+        { method: "DELETE" }
+      )
+      if (!response.ok) {
+        console.log("Failed to delete that Book"); 
+      }
+      const data = await response.json()
+      if (data) {
+        setSuccessMessage("Book Deleted Successfully.")
+        window.location.reload()
+      }
+    } catch (error) {
+      throw error
+    }
+  }
 
   return (
     <div>
       <h1>All books</h1>
+      {loading && <p>Loading...</p>}
       <ul>
         {data?.map((book) => (
-          <li key={book._id}>{book.title}</li>
+          <li key={book._id}>
+            {book.title}
+            <button onClick={() => deleteABook(book._id)}>Delete</button>
+          </li>
         ))}
       </ul>
+      <p>{successMessage}</p>
     </div>
   )
 }
